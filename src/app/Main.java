a78b7dfe11e22d4da4246da7186f4a8c45be9e5b
Dmitry <dmitrych5129@gmail.com>
2024-07-05 11:28:40 +0300

    Sums of numbers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,60 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Shop-online for customers");
+       SumNumbers sumNumb = new SumNumbers();
+       Print print = new Print();
+       Scanner scanner = new Scanner(System.in);
+
+       print.printTextFrom();
+       int scanNumb1 = scanner.nextInt();
+       print.printTextTo();
+       int scanNumb2 = scanner.nextInt();
+
+       sumNumb.setFrom(scanNumb1);
+       sumNumb.setTo(scanNumb2);
+       sumNumb.сountSum();
+
+       print.printSumNumb(sumNumb);
     }
 }
+
+class SumNumbers {
+    private int  sum;
+    private int  from;
+    private int  to;
+
+    public void setFrom(int from) {
+           this.from = from;
+    }
+    public void setTo(int to) {
+        this.to = to;
+    }
+    public int getSum() {
+        return sum;
+    }
+
+    public void сountSum(){
+        for(int i = from; i <= to; i++) {
+            System.out.print(i + ")" + " Num is " + i + ", ");
+            sum += i;
+            System.out.println("sum is " + sum);
+        }
+    }
+}
+
+class Print {
+    public void printTextFrom(){
+        System.out.print("Enter range of numbers!" + "\n" + "From: ");
+    }
+    public void printTextTo(){
+        System.out.print("To: " );
+    }
+    public void printSumNumb(SumNumbers sumNumb) {
+        System.out.println("________________________");
+        System.out.println("Sum of numbers is " + sumNumb.getSum());
+    }
+}
+
