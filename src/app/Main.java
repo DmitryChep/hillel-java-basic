59e5a7bb51d7fa783f69ec9eb837eecd377f884c
Dmitry <dmitrych5129@gmail.com>
2024-08-23 13:13:45 +0300

    Practice Maven and unit testing

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,51 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Shop-online for customers");
+        Calculator calculator = new Calculator(2,4);
+
+
+
     }
+}
+class Calculator {
+    private int number1;
+    private int number2;
+
+    public Calculator (int number1, int number2){
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int additionNumbers() {
+        return number1 + number2;
+    }
+
+    public int subtractionNumbers() {
+        return number1 - number2;
+    }
+
+    public int multiplicationNumbers() {
+        return number1 * number2;
+    }
+
+    public int divisionNumbers() {
+        return number1 / number2;
+    }
+
 }
