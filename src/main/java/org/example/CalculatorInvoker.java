0b1c96833a792a831cf04ad03807b13506af64a9
Dmitry <dmitrych5129@gmail.com>
2024-08-25 08:25:52 +0300

    Maven practice and unit test

---
+++ src/main/java/org/example/CalculatorInvoker.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import app.Calculator;
+
+
+public class CalculatorInvoker {
+    public static void main(String[] args) {
+        Calculator.main(new String[0]);
+    }
+}
